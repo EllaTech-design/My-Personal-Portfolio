0a2bf1c1fe804fe618d550189232e0e3da8c5a4f
EllaTech-design <emmanuellaukamaka079@gmail.com>
2025-08-29 05:05:10 -0700

    save changes in all

--- script.js
+++ script.js
@@ -12,7 +12,39 @@ function opentab(tabname){
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
-/*.......Menu ......... */
+// Typing animation
+const headerAnimate = document.querySelector(".header-animate");
+const textArray = ["FRONTEND DEVELOPER", "FULL TIME SOFTWARE DEVELOPER"];
+let textIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+  if (charIndex < textArray[textIndex].length) {
+    headerAnimate.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(eraseEffect, 1500);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    headerAnimate.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 50);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(typeEffect, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(typeEffect, 500);
+});
+
+
+//    Menu 
 var menu = document.getElementById("menu");
 function openmenu(){
   menu.style.right="0"
